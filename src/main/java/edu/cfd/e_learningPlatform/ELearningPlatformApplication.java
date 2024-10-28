074722f99b94fe6a332cbbb5046f28f6a5a0e17e
trinhdongkhanhwork <trinhdongkhanh.work@gmail.com>
2024-10-29 06:43:39 +0700

    create: fist-comit create new project for manager git

---
+++ src/main/java/edu/cfd/e_learningPlatform/ELearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package edu.cfd.e_learningPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ELearningPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ELearningPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cfd/e_learningPlatform/ELearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cfd.e_learningPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ELearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
