aef714bb8127e975217ffd55595352a7cd7a4e98
Nguyễn Quốc Khánh <nguyenkhanhahihivl@gmail.com>
2025-04-05 18:10:23 +0700

    feat: Bình luận video bài học - Chỉnh sủa một số lỗi của tính năng nhắn tin

--- src/main/java/edu/cfd/e_learningPlatform/controller/CommentController.java
+++ src/main/java/edu/cfd/e_learningPlatform/controller/CommentController.java
@@ -1,7 +1,8 @@
 package edu.cfd.e_learningPlatform.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import edu.cfd.e_learningPlatform.dto.request.CommentRequest;
@@ -11,6 +12,8 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
+import java.util.List;
+
 @RestController()
 @RequestMapping("/comments")
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -18,21 +21,25 @@ import lombok.experimental.FieldDefaults;
 public class CommentController {
 
     CommentService commentService;
+    SimpMessagingTemplate messagingTemplate;
+
+    @GetMapping("/get/{idVideo}")
+    public ResponseEntity<List<CommentVideoResponse>> fetchComment(@PathVariable Long idVideo) {
+        return ResponseEntity.ok(commentService.getCommentVideo(idVideo));
+    }
 
     @MessageMapping("/comments/post")
-    @SendTo("/topic/comments")
-    public CommentVideoResponse addComment(@RequestBody CommentRequest commentRequest) {
-        return commentService.addComment(commentRequest);
+    public void addComment(@RequestBody CommentRequest commentRequest) {
+        CommentVideoResponse response = commentService.addComment(commentRequest);
+        messagingTemplate.convertAndSend("/comment/" + commentRequest.getVideoId() + "/private", response);
     }
 
     @MessageMapping("/comments/update")
-    @SendTo("/topic/comments")
     public CommentVideoResponse updateComment(@RequestBody CommentRequest commentRequest) {
         return commentService.updateComment(commentRequest);
     }
 
     @MessageMapping("/comments/delete")
-    @SendTo("/topic/idComment")
     public Long deleteComment(@RequestBody CommentVideoResponse commentVideoResponse) {
         commentService.deleteComment(commentVideoResponse.getId());
         return commentVideoResponse.getId();
