81a4ebe7a2b25cacaeebbd96b06ca9aa8c0441c2
trinhdongkhanhwork <trinhdongkhanh.work@gmail.com>
2025-05-03 11:49:03 +0700

    feat(chung chi)

--- src/main/java/edu/cfd/e_learningPlatform/service/Impl/CertificateService.java
+++ src/main/java/edu/cfd/e_learningPlatform/service/Impl/CertificateService.java
@@ -3,6 +3,7 @@ package edu.cfd.e_learningPlatform.service.Impl;
 import edu.cfd.e_learningPlatform.dto.response.CertificateResponse;
 import edu.cfd.e_learningPlatform.entity.*;
 import edu.cfd.e_learningPlatform.repository.*;
+import edu.cfd.e_learningPlatform.service.CourseService;
 import edu.cfd.e_learningPlatform.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -16,6 +17,7 @@ public class CertificateService {
 
     private final CertificateRepository certificateRepository;
     private final UserService userService;
+    private final CourseService courseService;
 
 
     public List<CertificateResponse> getCertificatesForCurrentUser() {
@@ -25,8 +27,7 @@ public class CertificateService {
         return certificates.stream()
                 .map(cert -> new CertificateResponse(
                         cert.getId(),
-                        cert.getCourse().getId(),
-                        cert.getCourse().getTitle(),
+                        courseService.getCourseById(cert.getCourse().getId()),
                         cert.getIssuedAt()
                 ))
                 .collect(Collectors.toList());
